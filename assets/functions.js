e3f3e951c11e7fab6d9c10ec9011ef9b951d9599
Victor Perez <vjperez@mix.wvu.edu>
2018-05-27 20:37:09 -0400

    Mobile design finished

--- assets/functions.js
+++ assets/functions.js
@@ -1,5 +1,37 @@
-var overlay = document.querySelector('overlay');
+var fullImg = document.querySelector('.full-img');
+var thumbBar = document.querySelector('.thumb-bar');
+var imgCont = document.querySelector('.images');
 
-overlay.onclick = function() {
-  document.overlay.style.backgroundColor = rgba(0, 0, 0, 0.5);
+var images = ["sleep.gif","grill.png", "bloodlust.png", "epilepsy.gif", 
+            "trees.gif", "fun.png","bear.png","group.png",
+            "rocket.gif", "swirl.gif", "gummy.png", "hair.gif","profilePhoto.png"];
+/* Looping through images */
+
+for (var i = 0; i < 4; i++) {
+  var newImg = document.createElement('img');
+  newImg.setAttribute('src', 'assets/images/'+images[i]);
+  if (i == 0) {
+    fullImg.appendChild(newImg);
+    fullImg = newImg;
+  } else
+    thumbBar.appendChild(newImg);
+  newImg.onclick = function (e) {
+    var imgSrc = e.target.getAttribute('src');
+    e.target.setAttribute('src', fullImg.getAttribute('src'));
+    displayImage(imgSrc);
+  }
+}
+
+function displayImage(imgSrc) {
+  fullImg.setAttribute('src', imgSrc);
+
+} 
+     
+      
+     
+/* Add the rest of the images */
+for (var i = 4; i < images.length; i++) {
+  newImg = document.createElement('img');
+  newImg.setAttribute('src', 'assets/images/' + images[i]);
+  imgCont.appendChild(newImg);
 }
